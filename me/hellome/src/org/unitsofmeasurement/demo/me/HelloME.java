1b032eb68f0e24263ccb292c886dda3eee05a6f4
keilw <werner.keil@gmx.net>
2014-01-24 22:38:06 +0100

    ASSIGNED - bug 366909: Code Examples https://bugs.eclipse.org/bugs/show_bug.cgi?id=366909

--- me/hellome/src/org/unitsofmeasurement/demo/me/HelloME.java
+++ me/hellome/src/org/unitsofmeasurement/demo/me/HelloME.java
@@ -1,30 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- *//*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright (c) 2014 Werner Keil and others.
+ * All rights reserved.
+ *
+ * See LICENSE.txt for details.
  */
 
 package org.unitsofmeasurement.demo.me;
 
+import static org.unitsofmeasurement.impl.enums.unit.TemperatureUnit.*;
+
 import javax.measure.quantity.Temperature;
 import javax.microedition.midlet.MIDlet;
 import org.unitsofmeasurement.impl.enums.quantity.TemperatureQuantity;
-import org.unitsofmeasurement.impl.enums.unit.TemperatureUnit;
+
 
 /**
- *
  * @author Werner Keil
  */
 public class HelloME extends MIDlet {
     
     public void startApp() {
         
-        Temperature t =  new TemperatureQuantity(23.5, TemperatureUnit.CELSIUS);
-        //System.out.println("Hello Java ME");
+        Temperature t =  new TemperatureQuantity(23.5, CELSIUS);
         System.out.println("Temperature: " + t);
     }
     
