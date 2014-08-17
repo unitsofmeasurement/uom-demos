40803e459be3b56e886a2d6da5abe8d7110cee07
Werner Keil <werner.keil@gmx.net>
2014-08-17 20:04:19 +0200

    5: Improve demos Updated to latest tag Task-Url: https://github.com/unitsofmeasurement/unit-demos/issues/issue/5

---
+++ console/se/src/main/java/org/unitsofmeasurement/demo/se/lambda/StreamDemo.java
@@ -0,0 +1,18 @@
+package org.unitsofmeasurement.demo.se.lambda;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+import javax.measure.Unit;
+
+import org.unitsofmeasurement.impl.util.SI;
+
+public class StreamDemo {
+
+	public static void main(String[] args) {
+		//Set<Unit> units = SI.getInstance().getUnits();
+		//Stream<String> names = units.stream().map(Unit::getName);
+
+	}
+
+}
