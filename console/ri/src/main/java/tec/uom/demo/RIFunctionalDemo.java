625ecd4ee827de347bafff1d78b591c53721aa7e
Werner Keil <werner.keil@gmx.net>
2014-08-26 01:53:07 +0200

    UNITSOFMEASUREMENT-49: Specialize ConversionOperator Updated converter related demos Task-Url: https://java.net/jira/browse/UNITSOFMEASUREMENT-49

---
+++ console/ri/src/main/java/tec/uom/demo/RIFunctionalDemo.java
@@ -0,0 +1,30 @@
+package tec.uom.demo;
+
+import javax.measure.Quantity;
+import javax.measure.Unit;
+import javax.measure.function.ConversionOperator;
+import javax.measure.quantity.Length;
+
+import tec.units.ri.AbstractQuantity;
+import tec.units.ri.util.SI;
+import tec.units.ri.util.US;
+
+public class RIFunctionalDemo {
+
+	public static void main(String[] args) {
+//		ConversionOperator<String, Integer> converter = (from) -> Integer.valueOf(from);
+//		Integer converted = converter.to("123");
+//		System.out.println(converted); // 123
+		
+//		ConversionOperator<Unit<Length>, Quantity<Length>> converter2 = (from) -> AbstractQuantity.of(10, SI.METRE);
+//		Quantity<Length> converted2 = converter2.to(US.INCH);
+//		System.out.println(converted2); // XYZ
+//		Quantity<Length> converted3 = converter2.to(US.FOOT);
+//		System.out.println(converted3);
+		//Quantity<Length> conv2 = (from) -> Integer.valueOf(from);
+		
+		Quantity<Length> len = AbstractQuantity.of(10, SI.METRE);
+		Quantity<Length> len2 = len.to(US.FOOT);
+		System.out.println(len2);
+	}
+}
--- console/se/src/main/java/tec/uom/demo/se/lambda/FunctionalDemo.java
+++ console/se/src/main/java/tec/uom/demo/se/lambda/FunctionalDemo.java
@@ -8,7 +8,6 @@ import javax.measure.quantity.Length;
 import tec.uom.se.AbstractQuantity;
 import tec.uom.se.util.SI;
 import tec.uom.se.util.US;
-import tec.uom.se.model.*;
 
 public class FunctionalDemo {
 
@@ -17,12 +16,15 @@ public class FunctionalDemo {
 		Integer converted = converter.to("123");
 		System.out.println(converted); // 123
 		
-		
 		ConversionOperator<Unit<Length>, Quantity<Length>> converter2 = (from) -> AbstractQuantity.of(10, SI.METRE);
 		Quantity<Length> converted2 = converter2.to(US.INCH);
 		System.out.println(converted2); // XYZ
-		
-		
+		Quantity<Length> converted3 = converter2.to(US.FOOT);
+		System.out.println(converted3);
 		//Quantity<Length> conv2 = (from) -> Integer.valueOf(from);
+		
+		Quantity<Length> len = AbstractQuantity.of(10, SI.METRE);
+		Quantity<Length> len2 = len.to(US.FOOT);
+		System.out.println(len2);
 	}
 }
