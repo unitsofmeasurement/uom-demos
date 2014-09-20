a5dbc1c159e57578d2ee9fcd426b5cd32658cb9e
Werner Keil <werner.keil@gmx.net>
2014-09-20 15:29:10 +0200

    5: Improve demos
    Task-Url:
    https://github.com/unitsofmeasurement/unit-demos/issues/issue/5

--- javame/medemo/src/medemo/JavaMEApplication.java
+++ javame/medemo/src/medemo/JavaMEApplication.java
@@ -14,16 +14,11 @@
  *  limitations under the License.
  */
 
-package javameapplication;
+package medemo;
 
-import java.util.Iterator;
-import javax.measure.Measurement;
 import javax.measure.Quantity;
-import javax.measure.quantity.Mass;
 import javax.measure.quantity.Temperature;
 import javax.microedition.midlet.MIDlet;
-import javax.microedition.rms.RecordEnumeration;
-import javax.microedition.rms.RecordStore;
 /*
 import tec.uom.impl.enums.quantity.TemperatureAmount;
 import tec.uom.impl.enums.quantity.TimeAmount;
@@ -36,47 +31,25 @@ import org.unitsofmeasurement.impl.enums.unit.TemperatureUnit;
 import org.unitsofmeasurement.impl.enums.unit.TimeUnit;
 import org.unitsofmeasurement.impl.enums.quantity.TemperatureQuantity;
 
-//import tec.units.ri.quantity.QuantityFactory;
-//import tec.units.ri.util.SI;
 
 /**
- *
+ * ME Demo using JSR 363
  * @author Werner
  */
 public class JavaMEApplication extends MIDlet {
-    
-   // private Display display;
-    private String appName = "MEDemo";
-    private boolean firstTime;
-
-    private Measurement measure;
+    private final String appName = "MEDemo";
     private Quantity quantity;
-    private byte[] saveBuf;     // buffer for saved data
-    private int saveBufIndex = 0;
-    private int saveBufLength = 10;
-    private RecordStore store;  // record store, null if not open
-    
+  
     public void startApp() {
-        /*
-        Iterator<Display> disps = Display.getDisplays(false);
-        while(disps.hasNext()) {
-            Display d = disps.next();
-            if (display == null) display = d;
-        } */
-        
-        //measure = ShirtSizeEnum.L;
-        //quantity = QuantityFactory.getInstance(Mass.class).create(10, SI.KILOGRAM);
-        //quantity = new AreaQuantity(10d, AreaUnit.sqmetre);
-        
-        quantity = new TimeQuantity(10d, TimeUnit.MINUTE);
         //quantity = new TimeAmount(10d, TimeUnit.MINUTE);
+        quantity = new TimeQuantity(10d, TimeUnit.MINUTE);
         
+        //Quantity<Temperature> temp = new TemperatureAmount(10d,
         Quantity<Temperature> temp = new TemperatureQuantity(10d,
-        //Quantity<Temperature> temp = new TemperatureAmount(10d, 
                 TemperatureUnit.CELSIUS);
+
         System.out.println();
-        System.out.println();
-        System.out.println("Hello ME"); 
+        System.out.println("Hello " + appName); 
         System.out.println();
         System.out.println(quantity);
         System.out.println(temp);
@@ -84,8 +57,7 @@ public class JavaMEApplication extends MIDlet {
     
     public void pauseApp() {
     }
-    
+   
     public void destroyApp(boolean unconditional) {
     }
-
 }
