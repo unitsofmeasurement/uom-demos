8aea3212dbe35d9c2433580635a762dcdf815e80
Werner Keil <werner.keil@gmx.net>
2014-10-17 19:45:37 +0200

    5: Improve demos Add a simple SmartHome demo Task-Url: https://github.com/unitsofmeasurement/uom-demos/issues/issue/5

---
+++ console/ri/src/main/java/tec/uom/demo/SmartHomeDemo.java
@@ -0,0 +1,19 @@
+package tec.uom.demo;
+
+import javax.measure.Quantity;
+import javax.measure.quantity.Temperature;
+import javax.measure.quantity.Volume;
+
+import tec.units.ri.quantity.Quantities;
+import tec.units.ri.util.SI;
+
+public class SmartHomeDemo {
+
+	public static void main(String[] args) {
+		Quantity<Volume> volume = Quantities.getQuantity(1000, SI.LITRE);
+		Quantity<Temperature> temperature = Quantities.getQuantity(20, SI.KELVIN);
+		Quantity<?> work = (volume.multiply(temperature).multiply(4200)).divide(3600);
+		System.out.println(work);
+	}
+
+}
