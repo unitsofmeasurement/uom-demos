bfb0c41573fb5e1577d62dfb21d12e6386ae99d6
Werner Keil <werner.keil@gmx.net>
2014-11-03 00:47:27 +0100

    UNITSOFMEASUREMENT-65: Move QuantityFactory from "function" to "spi"
    Task-Url: https://java.net/jira/browse/UNITSOFMEASUREMENT-65

--- console/se/src/main/java/tec/uom/demo/se/FactoryDemo.java
+++ console/se/src/main/java/tec/uom/demo/se/FactoryDemo.java
@@ -5,6 +5,7 @@ import javax.measure.function.QuantityFactory;
 import javax.measure.quantity.Length;
 import javax.measure.quantity.Mass;
 
+import tec.uom.se.quantity.QuantityFactoryProvider;
 import tec.uom.se.quantity.internal.OldProxyQuantityFactory;
 //import tec.uom.se.quantity.ProxyQuantityFactory;
 import tec.uom.se.util.SI;
@@ -18,8 +19,8 @@ public class FactoryDemo {
 //		Quantity<Length> len2 = len.multiply(2);
 //		System.out.println(len2);
 		
-//		QuantityFactory<Mass> massFactory = ProxyQuantityFactory.getInstance(Mass.class);
-//		Quantity<Mass> mass = massFactory.create(50, SI.KILOGRAM);
-//		System.out.println(mass);
+		QuantityFactory<Mass> massFactory = QuantityFactoryProvider.getQuantityFactory(Mass.class);
+		Quantity<Mass> mass = massFactory.create(50, SI.KILOGRAM);
+		System.out.println(mass);
 	}
 }
