efe84df6904b837f22aa11b94b243f5f61e07a97
Werner Keil <werner.keil@gmx.net>
2014-12-16 13:08:33 +0100

    Update SmartHomeDemo.java
    Updated JavaDoc

--- console/ri/src/main/java/tec/uom/demo/SmartHomeDemo.java
+++ console/ri/src/main/java/tec/uom/demo/SmartHomeDemo.java
@@ -8,6 +8,11 @@ import javax.measure.quantity.Volume;
 import tec.units.ri.quantity.Quantities;
 import tec.units.ri.util.SI;
 
+/**
+ * 
+ * @author Werner
+ * @see <a href="http://www.dagego.de/info_waermebedarf.html">Dageto Wärmebedarfsermittlung (DE)</a>
+ */
 public class SmartHomeDemo {
 
 	public static void main(String[] args) {
