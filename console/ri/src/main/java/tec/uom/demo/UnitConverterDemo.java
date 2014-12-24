96781e0207d3cb708b7718a1c0c01648327413f1
Werner Keil <werner.keil@gmx.net>
2014-12-24 04:28:26 +0100

    UNITSOFMEASUREMENT-102: Wrong conversion factor of RI UnitConverter
    Task-Url: https://java.net/jira/browse/UNITSOFMEASUREMENT-102

--- console/ri/src/main/java/tec/uom/demo/UnitConverterDemo.java
+++ console/ri/src/main/java/tec/uom/demo/UnitConverterDemo.java
@@ -1,3 +1,18 @@
+/**
+ *  Unit-API - Units of Measurement API for Java
+ *  Copyright 2013-2014, Jean-Marie Dautelle, Werner Keil, V2COM and individual
+ *  contributors by the @author tag.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package tec.uom.demo;
 
 import static tec.units.ri.util.SI.*;
@@ -9,13 +24,11 @@ import javax.measure.UnitConverter;
 import javax.measure.quantity.Length;
 import javax.measure.quantity.Mass;
 
-import tec.units.ri.function.RationalConverter;
 import tec.units.ri.quantity.Quantities;
 
 public class UnitConverterDemo {
 
 	public static void main(String[] args) {
-		UnitConverter c1 = new RationalConverter(1, 10^2); 
 		Unit<Length> sourceUnit = METRE;
 		Unit<Length> targetUnit = CENTI(METRE);
 		UnitConverter converter = sourceUnit.getConverterTo(targetUnit);
@@ -25,15 +38,15 @@ public class UnitConverterDemo {
 		double result2 = converter.convert(length2);
 		System.out.println(result1);
 		System.out.println(result2);
-//		Quantity<Length> quantLength1 = Quantities.getQuantity(4.0, sourceUnit);
-//		Quantity<Length> quantLength2 = Quantities.getQuantity(6.0, targetUnit);
-//		Quantity<Length> quantResult1 = quantLength1.to(targetUnit);
-//		System.out.println(quantResult1);
-//		
-//		double mass1 = 5.0;
-//		double result3 = converter.convert(mass1); // does compile
-//		System.out.println(result3);
-//		Quantity<Mass> quantMass1 = Quantities.getQuantity(5.0, KILOGRAM);
+		Quantity<Length> quantLength1 = Quantities.getQuantity(4.0, sourceUnit);
+		Quantity<Length> quantLength2 = Quantities.getQuantity(6.0, targetUnit);
+		Quantity<Length> quantResult1 = quantLength1.to(targetUnit);
+		System.out.println(quantResult1);
+		
+		double mass1 = 5.0;
+		double result3 = converter.convert(mass1); // does compile
+		System.out.println(result3);
+		Quantity<Mass> quantMass1 = Quantities.getQuantity(5.0, KILOGRAM);
 		//quantMass1.to(targetUnit); // won't compile
  	}
 }
