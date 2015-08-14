0ed0f79dfbb740fc95c46551f5b26d9963018d05
Werner Keil <werner.keil@gmx.net>
2015-08-14 16:24:00 +0200

    Update CommonFunctionalDemo.java
    No Lambdas in RI

--- console/systems/common/src/main/java/tec/uom/demo/systems/common/CommonFunctionalDemo.java
+++ console/systems/common/src/main/java/tec/uom/demo/systems/common/CommonFunctionalDemo.java
@@ -10,17 +10,6 @@ import tec.units.ri.unit.Units;
 public class CommonFunctionalDemo {
 
 	public static void main(String[] args) {
-//		ConversionOperator<String, Integer> converter = (from) -> Integer.valueOf(from);
-//		Integer converted = converter.to("123");
-//		System.out.println(converted); // 123
-		
-//		ConversionOperator<Unit<Length>, Quantity<Length>> converter2 = (from) -> AbstractQuantity.of(10, Units.METRE);
-//		Quantity<Length> converted2 = converter2.to(US.INCH);
-//		System.out.println(converted2); // XYZ
-//		Quantity<Length> converted3 = converter2.to(US.FOOT);
-//		System.out.println(converted3);
-		//Quantity<Length> conv2 = (from) -> Integer.valueOf(from);
-		
 		Quantity<Length> len = NumberQuantity.of(10, Units.METRE);
 		Quantity<Length> len2 = len.to(US.FOOT);
 		System.out.println(len2);
