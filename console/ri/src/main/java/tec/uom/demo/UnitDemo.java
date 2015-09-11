079b77c4269be455ec63bb3634ff31f735392fd3
Werner Keil <werner.keil@gmx.net>
2015-09-11 19:44:31 +0200

    23: Enable CI
    Task-Url:
    https://github.com/unitsofmeasurement/uom-demos/issues/issue/23

--- console/ri/src/main/java/tec/uom/demo/UnitDemo.java
+++ console/ri/src/main/java/tec/uom/demo/UnitDemo.java
@@ -1,9 +1,38 @@
+/*
+ *  Unit-API - Units of Measurement API for Java
+ *  Copyright (c) 2005-2015, Jean-Marie Dautelle, Werner Keil, V2COM.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-363, Unit-API nor the names of their contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package tec.uom.demo;
+
+import static tec.units.ri.unit.MetricPrefix.NANO;
+import static tec.units.ri.unit.Units.LITRE;
 
 import javax.measure.Unit;
 import javax.measure.quantity.Dimensionless;
 import javax.measure.quantity.Speed;
 
+import tec.units.ri.quantity.Quantities;
 import tec.units.ri.unit.MetricPrefix;
 import tec.units.ri.unit.Units;
 
@@ -30,8 +59,11 @@ public class UnitDemo {
 		System.out.println(Units.GRAM.getSymbol());
 		System.out.println(Units.GRAM.getName());
 		
-		Unit<Dimensionless> d = Units.METRE.asType(Dimensionless.class);
-		System.out.println(d);
+//		Unit<Dimensionless> d = Units.METRE.asType(Dimensionless.class);
+//		System.out.println(d);
+		
+		System.out.println(Quantities.getQuantity(3.3, LITRE).toString());
+		System.out.println(Quantities.getQuantity(3.3, NANO(LITRE)).toString());
 	}
 
 }
--- console/systems/common/src/main/java/tec/uom/demo/systems/common/CommonPrefixDemo.java
+++ console/systems/common/src/main/java/tec/uom/demo/systems/common/CommonPrefixDemo.java
@@ -1,12 +1,17 @@
 package tec.uom.demo.systems.common;
 
 import static tec.units.ri.unit.MetricPrefix.KILO;
+import static tec.units.ri.unit.MetricPrefix.NANO;
+import static systems.uom.common.US.LITER;
 
 import javax.measure.Quantity;
+import javax.measure.format.UnitFormat;
 import javax.measure.quantity.Length;
 import javax.measure.quantity.Mass;
+import javax.measure.quantity.Volume;
 
 import systems.uom.common.IndianPrefix;
+import tec.units.ri.format.EBNFUnitFormat;
 import tec.units.ri.quantity.Quantities;
 import tec.units.ri.unit.Units;
 
@@ -16,5 +21,11 @@ public class CommonPrefixDemo {
 		System.out.println(len);
 		Quantity<Mass> kg = Quantities.getQuantity(50, KILO(Units.GRAM));
 		System.out.println(kg);
+		
+		System.out.println(Quantities.getQuantity(3.3, LITER).toString());
+		Quantity<Volume> nl = Quantities.getQuantity(3.3, NANO(LITER));
+		System.out.println(nl.toString());
+		UnitFormat format = EBNFUnitFormat.getInstance();
+		System.out.println(format.format(nl.getUnit()));
 	}
 }
