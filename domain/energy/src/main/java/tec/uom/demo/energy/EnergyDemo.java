64d602d3ba1739532f9db72e1f3b3ca77be1b657
Werner Keil <werner.keil@gmx.net>
2015-10-16 01:06:01 +0200

    95: Consolidate message bundle files
    Task-Url:
    https://github.com/unitsofmeasurement/uom-se/issues/issues/issue/95

---
+++ domain/energy/src/main/java/tec/uom/demo/energy/EnergyDemo.java
@@ -0,0 +1,28 @@
+package tec.uom.demo.energy;
+
+import static tec.uom.demo.energy.DemoUnitSystem.WATTHOUR;
+
+import javax.measure.Quantity;
+import javax.measure.quantity.Energy;
+import javax.measure.quantity.Power;
+import javax.measure.quantity.Time;
+
+import tec.units.ri.quantity.Quantities;
+import tec.units.ri.unit.MetricPrefix;
+import tec.units.ri.unit.Units;
+
+public class EnergyDemo {
+
+	public static void main(String[] args) {
+		//System.out.println(DemoUnitSystem.WATTHOUR);
+		Quantity<Power> power = Quantities.getQuantity(1000, MetricPrefix.MILLI(Units.WATT));
+		Quantity<Time> time = Quantities.getQuantity(15, Units.MINUTE);
+		Quantity<Energy> energy = power.multiply(time).asType(Energy.class);
+		System.out.println(energy); //Ouput 15000.00 J/s·min -> correct
+		System.out.println(energy.to(Units.JOULE)); //Output 900.00 J -> correct
+		System.out.println(energy.to(WATTHOUR)); //Output 0.25 m·N -> Expected: 0.25 Wh
+		System.out.println(energy.to(MetricPrefix.KILO(Units.JOULE))); //Output 0.90 m·N -> Wrong, expected something like 0.90 kJ or 0.90 kNm
+		Quantity<Energy> result = energy.to(MetricPrefix.KILO(WATTHOUR));
+		System.out.println(result); //Output 0.00025 m·N -> Wrong, expected 0.00025 kWh
+	}
+}
