62a36bb4434371735c0231edf303a566946570a6
Werner Keil <werner.keil@gmx.net>
2015-10-20 20:01:12 +0200

    10: Add energy demos
    Task-Url:
    https://github.com/unitsofmeasurement/uom-demos/issues/issue/10

--- domain/energy-se/src/main/java/tec/uom/demo/energy/se/DemoUnitSystem.java
+++ domain/energy-se/src/main/java/tec/uom/demo/energy/se/DemoUnitSystem.java
@@ -1,8 +1,35 @@
+/*
+ *  Unit-API - Units of Measurement API for Java
+ *  Copyright (c) 2005-2015, Jean-Marie Dautelle, Werner Keil, V2COM.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-363, Unit-API nor the names of their contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package tec.uom.demo.energy.se;
 
 import static tec.uom.se.unit.MetricPrefix.*;
+
 import javax.measure.Unit;
 import javax.measure.quantity.Energy;
+
 import tec.uom.se.AbstractSystemOfUnits;
 import tec.uom.se.format.SimpleUnitFormat;
 import tec.uom.se.function.RationalConverter;
@@ -18,7 +45,7 @@ import tec.uom.se.unit.Units;
  * @noextend This class is not intended to be extended by clients.
  * 
  * @author <a href="mailto:units@catmedia.us">Werner Keil</a>
- * @version 0.2, $Date: 2015-10-16 $
+ * @version 0.3, $Date: 2015-10-20 $
  */
 public final class DemoUnitSystem extends AbstractSystemOfUnits {
     /**
@@ -28,7 +55,7 @@ public final class DemoUnitSystem extends AbstractSystemOfUnits {
     }
     
     /**
-     * The singleton instance of {@code Health}.
+     * The singleton instance of {@code DemoUnitSystem}.
      */
     private static final DemoUnitSystem INSTANCE = new DemoUnitSystem();
 
@@ -43,7 +70,8 @@ public final class DemoUnitSystem extends AbstractSystemOfUnits {
      * @param  unit the unit being added.
      * @return <code>unit</code>.
      */
-    private static <U extends Unit<?>>  U addUnit(U unit) {
+    @SuppressWarnings("unused")
+	private static <U extends Unit<?>>  U addUnit(U unit) {
         INSTANCE.units.add(unit);
         return unit;
     }
