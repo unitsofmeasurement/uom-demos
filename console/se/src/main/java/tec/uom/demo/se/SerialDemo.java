2fd03d3ee780baf4323f1284b7613a32ce27e095
Werner Keil <werner.keil@gmx.net>
2015-10-23 21:04:54 +0200

    96: Quantities are not serializable
    Task-Url:
    https://github.com/unitsofmeasurement/uom-se/issues/issues/issue/96

---
+++ console/se/src/main/java/tec/uom/demo/se/SerialDemo.java
@@ -0,0 +1,38 @@
+package tec.uom.demo.se;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import tec.uom.se.ComparableQuantity;
+import tec.uom.se.quantity.Quantities;
+import tec.uom.se.unit.Units;
+
+public class SerialDemo {
+	public static void main(String[] args) throws IOException,
+			ClassNotFoundException {
+		byte[] serialized = serialize(Quantities.getQuantity(0, Units.JOULE));
+		Object obj = deserialize(serialized);
+		ComparableQuantity<?> quantity = (ComparableQuantity<?>) obj;
+	}
+
+	public static Object deserialize(byte[] serialized) throws IOException,
+			ClassNotFoundException {
+		ByteArrayInputStream bis = new ByteArrayInputStream(serialized);
+		ObjectInputStream ois = new ObjectInputStream(bis);
+		Object obj = ois.readObject();
+		ois.close();
+		return obj;
+	}
+
+	public static byte[] serialize(ComparableQuantity<?> quantity)
+			throws IOException {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(bos);
+		oos.writeObject(quantity);
+		bos.close();
+		return bos.toByteArray();
+	}
+}
