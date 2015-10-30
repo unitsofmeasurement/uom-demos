4a464e88a7a5f8b8f940c318bbb52017633d4277
Werner Keil <werner.keil@gmx.net>
2015-10-30 09:02:47 -0700

    27: ME 8.2 integration
    Task-Url:
    https://github.com/unitsofmeasurement/uom-demos/issues/issue/27

--- console/systems/common/src/main/java/tec/uom/demo/systems/common/CommonPrefixDemo.java
+++ console/systems/common/src/main/java/tec/uom/demo/systems/common/CommonPrefixDemo.java
@@ -11,7 +11,7 @@ import javax.measure.quantity.Mass;
 import javax.measure.quantity.Volume;
 
 import systems.uom.common.IndianPrefix;
-import tec.units.ri.format.EBNFUnitFormat;
+import tec.units.ri.format.SimpleUnitFormat;
 import tec.units.ri.quantity.Quantities;
 import tec.units.ri.unit.Units;
 
@@ -25,7 +25,8 @@ public class CommonPrefixDemo {
 		System.out.println(Quantities.getQuantity(3.3, LITER).toString());
 		Quantity<Volume> nl = Quantities.getQuantity(3.3, NANO(LITER));
 		System.out.println(nl.toString());
-		UnitFormat format = EBNFUnitFormat.getInstance();
+		//UnitFormat format = EBNFUnitFormat.getInstance();
+		UnitFormat format = SimpleUnitFormat.getInstance();
 		System.out.println(format.format(nl.getUnit()));
 	}
 }
