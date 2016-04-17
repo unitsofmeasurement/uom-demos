f54ed52de8e52c5cb248b151f706af92663e7982
Werner Keil <werner.keil@gmx.net>
2016-04-17 02:36:54 +0200

    Updated demos
    Signed-off-by: Werner Keil <werner.keil@gmx.net>

---
+++ console/systems/common/src/main/java/tec/uom/demo/systems/common/Beerfest.java
@@ -0,0 +1,59 @@
+/*
+ *  Unit-API - Units of Measurement API for Java
+ *  Copyright (c) 2005-2016, Jean-Marie Dautelle, Werner Keil, V2COM.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the names of JSR-363, Units of Measurement nor the names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package tec.uom.demo.systems.common;
+
+import static tec.uom.demo.types.BeerUnits.*;
+
+import javax.measure.Quantity;
+import javax.measure.quantity.Volume;
+
+import systems.uom.common.Imperial;
+import systems.uom.common.US;
+import tec.units.ri.quantity.Quantities;
+
+public class Beerfest {
+
+	public static void main(String[] args) {
+		System.out.println("Ozapft is!");
+		System.out.println();
+		Quantity<Volume> v = Quantities.getQuantity(.5d, US.LITER);
+		System.out.println(v);
+		System.out.println(v.to(Imperial.PINT)); 
+		System.out.println(v.to(US.FLUID_OUNCE)); 
+		
+		System.out.println();
+		v = Quantities.getQuantity(1, MASS_HISTORIC);
+		System.out.println(v);
+		System.out.println(v.to(SCHOPPEN_BAYERN));
+		System.out.println(v.to(US.LITER));
+	}
+
+}
