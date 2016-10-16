19d0fe47b22715fc1806d554eff313e1400423ca
Werner Keil <werner.keil@gmx.net>
2016-10-16 19:05:50 +0200

    43: Integrate 1.0.1
    Task-Url: https://github.com/unitsofmeasurement/uom-systems/issues/issues/43

--- console/systems/common/src/main/java/tec/uom/demo/systems/common/CommonDemo.java
+++ console/systems/common/src/main/java/tec/uom/demo/systems/common/CommonDemo.java
@@ -4,12 +4,20 @@ import javax.measure.Unit;
 import javax.measure.spi.SystemOfUnits;
 
 import systems.uom.common.Imperial;
+import systems.uom.common.USCustomary;
 
 public class CommonDemo {
-    public static void main(String... args) {
-	SystemOfUnits sou = Imperial.getInstance();
-	for (Unit<?> u : sou.getUnits()) {
-	    System.out.println(u.getName() + "; " + u.getSymbol() + "; " + u);
+	public static void main(String... args) {
+		SystemOfUnits sou = Imperial.getInstance();
+		testSoU(sou);
+		sou = USCustomary.getInstance();
+		testSoU(sou);
 	}
-    }
+	
+	private static void testSoU(final SystemOfUnits sou) {
+		System.out.println("Testing " + sou.getName());
+		for (Unit<?> u : sou.getUnits()) {
+			System.out.println(u.getName() + "; " + u.getSymbol() + "; " + u);
+		}
+	}
 }
