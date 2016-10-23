19808c2a81f06210de7c5b33c5f62776bdf342fc
Werner Keil <werner.keil@gmx.net>
2016-10-23 18:32:31 +0200

    37: Update to 1.0.1

--- console/systems/si/src/main/java/tec/uom/demo/systems/si/SIDemo.java
+++ console/systems/si/src/main/java/tec/uom/demo/systems/si/SIDemo.java
@@ -1,7 +1,9 @@
 package tec.uom.demo.systems.si;
 
 import static tec.units.ri.unit.Units.*;
+import static si.uom.SI.*;
 
+import javax.measure.Quantity;
 import javax.measure.Unit;
 import javax.measure.quantity.Mass;
 
@@ -9,16 +11,17 @@ import tec.units.ri.AbstractQuantity;
 import tec.units.ri.quantity.NumberQuantity;
 
 public class SIDemo {
+    public static void main(String[] args) {
+	Unit<Mass> atomicMassUnit = UNIFIED_ATOMIC_MASS;
+	System.out.println(atomicMassUnit + " (" + atomicMassUnit.getName() + "; " + atomicMassUnit.getSymbol() + ")");
 
-	public static void main(String[] args) {
-		Unit<Mass> atomicMassUnit =  UNIFIED_ATOMIC_MASS;
-		System.out.println(atomicMassUnit.getSymbol());
+	AbstractQuantity<Mass> mass = NumberQuantity.of(10, atomicMassUnit);
+	System.out.println(mass);
 
-		AbstractQuantity<Mass> mass = NumberQuantity.of(10, atomicMassUnit);
-		System.out.println(mass);
-
-		AbstractQuantity<Mass> massInKg = mass.to(KILOGRAM);
-		System.out.println(massInKg);
-	}
-
+	Quantity<Mass> massInKg = mass.to(KILOGRAM);
+	System.out.println(massInKg);
+	
+	System.out.println(WATT_PER_STERADIAN);
+	System.out.println(WATT_PER_STERADIAN_PER_SQUARE_METRE);
+    }
 }
