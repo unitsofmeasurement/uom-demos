1d794a14ac897b891d712e57f7d1d39e6653d2c6
Werner Keil <werner.keil@gmx.net>
2017-03-24 20:06:44 +0100

    41: Add UCUM demos
    Task-Url: https://github.com/unitsofmeasurement/uom-demos/issues/issues/41

---
+++ console/systems/ucum/src/main/java/tec/uom/demo/systems/ucum/ReflectionDemo.java
@@ -0,0 +1,53 @@
+package tec.uom.demo.systems.ucum;
+
+import java.lang.reflect.ParameterizedType;
+
+import javax.measure.Quantity;
+import javax.measure.Unit;
+import javax.measure.quantity.Area;
+import javax.measure.quantity.Length;
+import javax.measure.quantity.Volume;
+
+import tec.uom.se.unit.Units;
+import systems.uom.ucum.UCUM;
+import tec.uom.se.unit.BaseUnit;
+
+public class ReflectionDemo {
+	public static void reflect(final Unit<?> aUnit) {
+//	public static <U extends Unit<Q>, Q extends Quantity<Q>> void reflect(final U aUnit) {
+		// Here I'd like to get the Class-Object 'Length'
+		System.out.print("aUnit: ");
+//		System.out.println(aUnit.getClass().getGenericInterfaces().length);
+		System.out.println(aUnit.getClass().getGenericSuperclass().getClass().getName());
+//		System.out.println(aUnit.getClass().getGenericSuperclass().getClass().getGenericInterfaces().length);
+		System.out.println(aUnit.getClass().getGenericSuperclass().getClass().getGenericInterfaces()[0]);
+		System.out.println(((ParameterizedType) aUnit.getClass()
+				.getGenericSuperclass()).getActualTypeArguments()[0]);
+		System.out.println(aUnit);
+
+//		System.out.print("anotherUnit: ");
+//		final Unit<Volume> anotherUnit = aUnit.multiply(aUnit).asType(Volume.class);
+//		System.out.println(anotherUnit.getClass().getGenericSuperclass());
+//		System.out.println(((ParameterizedType) anotherUnit.getClass()
+//				.getGenericSuperclass()).getActualTypeArguments()[0]);
+//		// System.out.println(((ParameterizedType) anotherUnit.getClass()
+//		// .getGenericSuperclass()).getActualTypeArguments().length);
+//		System.out.println(anotherUnit);
+	}
+
+	public static void main(String... args) {
+		reflect(new BaseUnit<Length>("m"));
+//		Unit<Length> len = new BaseUnit<Length>("m");
+		Unit<Length> len = Units.METRE;
+		reflect(len.multiply(len)); // this works under SE 8u20
+//		reflect(len.multiply(len).asType(Area.class));  // this won't compile under SE 8u20
+		
+//		for (Unit<?> u : Units.getInstance().getUnits()) {
+//			reflect(u);
+//		}
+		
+		for (Unit<?> u : UCUM.getInstance().getUnits()) {
+			reflect(u);
+		}
+	}
+}
