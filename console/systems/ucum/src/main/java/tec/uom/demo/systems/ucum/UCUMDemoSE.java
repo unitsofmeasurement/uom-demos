910e4e9efcbf902b330bef73f4235c9c6cc62eeb
Werner Keil <werner.keil@gmx.net>
2017-04-23 23:40:01 +0200

    97: UCUMFormat issues formatting multiples or submultiples
    Task-Url: https://github.com/unitsofmeasurement/uom-systems/issues/issues/97

--- console/systems/ucum/src/main/java/tec/uom/demo/systems/ucum/UCUMDemoSE.java
+++ console/systems/ucum/src/main/java/tec/uom/demo/systems/ucum/UCUMDemoSE.java
@@ -1,21 +1,20 @@
 package tec.uom.demo.systems.ucum;
 
+import static systems.uom.ucum.UCUM.ATOMIC_MASS_UNIT;
+import static tec.uom.se.unit.MetricPrefix.KILO;
+
 import javax.measure.Quantity;
 import javax.measure.Unit;
 import javax.measure.format.UnitFormat;
 import javax.measure.quantity.Frequency;
 import javax.measure.quantity.Mass;
-import javax.measure.spi.ServiceProvider;
 
 import systems.uom.ucum.UCUM;
 import systems.uom.ucum.format.UCUMFormat;
 import systems.uom.ucum.format.UCUMFormat.Variant;
 import tec.uom.se.format.EBNFUnitFormat;
 import tec.uom.se.quantity.Quantities;
-import tec.uom.se.unit.MetricPrefix;
 import tec.uom.se.unit.Units;
-import static systems.uom.ucum.UCUM.ATOMIC_MASS_UNIT;
-import static tec.uom.se.unit.MetricPrefix.KILO;
 
 public class UCUMDemoSE {
 
@@ -55,18 +54,5 @@ public class UCUMDemoSE {
 
 	UnitFormat ebnf = EBNFUnitFormat.getInstance();
 	unit = ebnf.parse("kHz");
-	System.out.println(unit);
-
-	System.out.println("Litering...");
-	final UnitFormat unitFormat = ServiceProvider.current().getUnitFormatService().getUnitFormat("CI");
-	final Unit<?> microliter = MetricPrefix.MICRO(Units.LITRE);
-	System.out.println(unitFormat.format(microliter)); // prints "nst"!
-
-	final Unit<?> microliter2 = unitFormat.parse("uL");
-	System.out.println(unitFormat.format(microliter2)); // prints "nst"!
-	
-	final UnitFormat unitFormat2 = ServiceProvider.current().getUnitFormatService().getUnitFormat("CS");
-	final Unit<?> microliter3 = unitFormat2.parse("ul");
-	System.out.println(unitFormat2.format(microliter3)); // prints "nst"!
-    }
+	System.out.println(unit);    }
 }
