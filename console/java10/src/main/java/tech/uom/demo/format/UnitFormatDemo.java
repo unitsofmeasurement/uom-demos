4705569e091b9764c0a8cae2450dc1a9d08e92b3
Werner Keil <werner.keil@gmx.net>
2019-01-29 00:44:56 +0100

    132: BinaryPrefixes not formatted properly by SimpleUnitFormat
    Task-Url: https://github.com/unitsofmeasurement/indriya/issues/issues/132

--- console/java10/src/main/java/tech/uom/demo/format/UnitFormatDemo.java
+++ console/java10/src/main/java/tech/uom/demo/format/UnitFormatDemo.java
@@ -1,6 +1,6 @@
 /*
  *  Unit-API - Units of Measurement API for Java
- *  Copyright (c) 2005-2016, Jean-Marie Dautelle, Werner Keil, V2COM.
+ *  Copyright (c) 2005-2019, Jean-Marie Dautelle, Werner Keil and others.
  *
  * All rights reserved.
  *
@@ -13,7 +13,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
  *    and the following disclaimer in the documentation and/or other materials provided with the distribution.
  *
- * 3. Neither the name of JSR-363, Unit-API nor the names of its contributors may be used to endorse or promote products
+ * 3. Neither the name of JSR-385, Units of Measurement nor the names of their contributors may be used to endorse or promote products
  *    derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
