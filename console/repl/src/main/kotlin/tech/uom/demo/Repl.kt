e41cac7c180f719b7b4daadde9df662619250990
David Stibbe <david@davidstibbe.nl>
2019-06-01 13:51:02 +0200

    Initial repl

---
+++ console/repl/src/main/kotlin/tech/uom/demo/Repl.kt
@@ -0,0 +1,26 @@
+package tech.uom.demo
+
+class Repl {
+}
+
+fun main(){
+    println("Hello World!")
+    while(true){
+        print("> ")
+        val input = readLine()
+        val output = process(input)
+        println("< You said: $output")
+    }
+}
+
+fun process(input: String?):String {
+    return if(input == null ) {
+        ""
+    }else{
+        parseAndEvaluate(input)
+    }
+}
+
+fun parseAndEvaluate(input: String):String {
+    return "succes"
+}
