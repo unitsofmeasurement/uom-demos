007d19ea283b03f971b8d6acab84afef41c8daa8
Werner Keil <werner.keil@gmx.net>
2019-07-20 12:26:20 +0300

    71: Experiment with Valhalla
    Task-Url: https://github.com/unitsofmeasurement/uom-demos/issues/issues/71

--- console/valhalla/src/main/java/tech/uom/demo/valhalla/ValhallaDemo.java
+++ console/valhalla/src/main/java/tech/uom/demo/valhalla/ValhallaDemo.java
@@ -1,10 +1,26 @@
 package tech.uom.demo.valhalla;
 
 public class ValhallaDemo {
-
+	private static final String PATTERN = "%s and %s are %s.";
+	private static final String SAME = "the same";
+	private static final String NOT_SAME = "not the same";
+	
 	public static void main(String[] args) {
-        VUnit u = new VUnit("kg","kilogram");
-        System.out.println(u);
+        VUnit m1 = new VUnit("kg","kilogram");
+        VUnit m2 = new VUnit("kg","kilogram");
+        System.out.println(m1);
+        if (m1 == m2) {
+        	System.out.println(String.format(PATTERN, m1, m2, SAME));
+        } else {
+        	System.out.println(String.format(PATTERN, m1, m2, NOT_SAME));
+        }
+        
+        VUnit m3 = new VUnit("g","gram");
+        if (m1 == m3) {
+        	System.out.println(String.format(PATTERN, m1, m3, SAME));
+        } else {
+        	System.out.println(String.format(PATTERN, m1, m3, NOT_SAME));
+        }
 	}
 
 }
