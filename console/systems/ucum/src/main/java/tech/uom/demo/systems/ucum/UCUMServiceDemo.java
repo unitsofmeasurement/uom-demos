70157ae2138ef5cd9693df93c514e08034b4f7d5
Werner Keil <werner.keil@gmx.net>
2020-06-12 23:01:21 +0200

    166: Digits not parsed correctly
    Task-Url: https://github.com/unitsofmeasurement/uom-systems/issues/166

--- console/systems/ucum/src/main/java/tech/uom/demo/systems/ucum/UCUMServiceDemo.java
+++ console/systems/ucum/src/main/java/tech/uom/demo/systems/ucum/UCUMServiceDemo.java
@@ -1,3 +1,28 @@
+/*
+ *  Units of Measurement Console Demos
+ *  Copyright (c) 2005-2020, Werner Keil and others.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-385, Unit-API nor the names of their contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package tech.uom.demo.systems.ucum;
 
 import java.util.List;
@@ -17,14 +42,14 @@ public class UCUMServiceDemo {
         System.out.println();
         System.out.println(ServiceProvider.current());
         
-        for (String formatName : ServiceProvider.current().getUnitFormatService().getAvailableFormatNames()) {
+        for (String formatName : ServiceProvider.current().getFormatService().getAvailableFormatNames()) {
             System.out.println(formatName);
         }
         
-    	UnitFormat unitFormat = ServiceProvider.current().getUnitFormatService().getUnitFormat("UCUM");
+    	UnitFormat unitFormat = ServiceProvider.current().getFormatService().getUnitFormat("UCUM");
     	System.out.println(unitFormat);
     	
-    	UnitFormat cs = ServiceProvider.current().getUnitFormatService().getUnitFormat("CS");
+    	UnitFormat cs = ServiceProvider.current().getFormatService().getUnitFormat("CS");
     	System.out.println(cs);
     	Unit<?> unit = cs.parse("m/s");
     	System.out.println(unit);
@@ -37,16 +62,15 @@ public class UCUMServiceDemo {
                 break;
             }
         }
-        final UnitFormat f = defaultProvider.getUnitFormatService().getUnitFormat();
+        final UnitFormat f = defaultProvider.getFormatService().getUnitFormat();
         System.out.println("Square m: " + f.parse("m^2"));
         
         List<ServiceProvider> providers = ServiceProvider.available();
         ServiceProvider ucumProvider = providers.get(0);
-        UnitFormatService ucumFormatService = ucumProvider.getUnitFormatService();
+        UnitFormatService ucumFormatService = ucumProvider.getFormatService();
         UnitFormat ucumFormatter = ucumFormatService.getUnitFormat("CS");
         System.out.println("m3 dimension =" + ucumFormatter.parse("m3").getDimension());        
         System.out.println("ft3 (wrong) dimension =" + ucumFormatter.parse("ft3").getDimension());
-        System.out.println("ft3 dimension =" + ucumFormatter.parse("[cft_i]").getDimension());
-        
+        System.out.println("ft3 dimension =" + ucumFormatter.parse("[cft_i]").getDimension());        
     }
 }
