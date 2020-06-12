be2a6bf9c00535fb095189669d26b7402518c705
Werner Keil <werner.keil@gmx.net>
2020-06-12 22:12:48 +0200

    166: Digits not parsed correctly
    Task-Url: https://github.com/unitsofmeasurement/uom-systems/issues/166

--- console/systems/ucum/src/main/java/tech/uom/demo/systems/ucum/UCUMServiceDemo.java
+++ console/systems/ucum/src/main/java/tech/uom/demo/systems/ucum/UCUMServiceDemo.java
@@ -1,8 +1,11 @@
 package tech.uom.demo.systems.ucum;
 
+import java.util.List;
+
 import javax.measure.Unit;
 import javax.measure.format.UnitFormat;
 import javax.measure.spi.ServiceProvider;
+import javax.measure.spi.UnitFormatService;
 
 public class UCUMServiceDemo {
 
@@ -36,5 +39,14 @@ public class UCUMServiceDemo {
         }
         final UnitFormat f = defaultProvider.getUnitFormatService().getUnitFormat();
         System.out.println("Square m: " + f.parse("m^2"));
+        
+        List<ServiceProvider> providers = ServiceProvider.available();
+        ServiceProvider ucumProvider = providers.get(0);
+        UnitFormatService ucumFormatService = ucumProvider.getUnitFormatService();
+        UnitFormat ucumFormatter = ucumFormatService.getUnitFormat("CS");
+        System.out.println("m3 dimension =" + ucumFormatter.parse("m3").getDimension());        
+        System.out.println("ft3 dimension =" + ucumFormatter.parse("ft3").getDimension());
+        System.out.println("ft3 dimension =" + ucumFormatter.parse("[cft_i]").getDimension());
+        
     }
 }
