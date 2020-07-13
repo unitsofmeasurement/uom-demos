9866d006433d8ddb9c4586f25716875ee1931a37
Andi Huber <ahuber@apache.org>
2020-07-13 08:23:47 +0200

    91: redoing this demo from scratch (improve the didactic value)

--- domain/energy/src/main/java/tech/uom/demo/energy/HeatRequirementDemo.java
+++ domain/energy/src/main/java/tech/uom/demo/energy/HeatRequirementDemo.java
@@ -25,9 +25,11 @@
  */
 package tech.uom.demo.energy;
 
-import static javax.measure.MetricPrefix.KILO;
-import static javax.measure.Quantity.Scale.*;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import javax.measure.Quantity;
+import javax.measure.Quantity.Scale;
 import javax.measure.Unit;
 import javax.measure.quantity.Energy;
 import javax.measure.quantity.Power;
@@ -35,44 +37,93 @@ import javax.measure.quantity.Temperature;
 import javax.measure.quantity.Time;
 import javax.measure.quantity.Volume;
 
+import static javax.measure.MetricPrefix.KILO;
+import static javax.measure.MetricPrefix.MILLI;
+
 import tech.units.indriya.format.SimpleUnitFormat;
+import tech.units.indriya.function.RationalNumber;
 import tech.units.indriya.quantity.Quantities;
 import tech.units.indriya.unit.Units;
-
-import java.util.Locale;
-import java.util.ResourceBundle;
 
 /**
  * 
  * @author Werner
+ * @author Andi Huber
  * @see <a href="http://www.dagego.de/info_waermebedarf.html">Dageto
  *      Wärmebedarfsermittlung (DE)</a>
- * @version 0.8
+ * @version 0.9
  */
 public class HeatRequirementDemo {
 
-	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
-		//setting default locale to German
-		Locale.setDefault(Locale.GERMAN);
-		final ResourceBundle resourceBundle = ResourceBundle.getBundle("Labels");
+		
+	    // When 1 cal of heat energy is transferred to one gram (1 g) of pure liquid water, 
+	    // the temperature of that sample of water is raised by one degree Celsius (1º C) 
+	    // or one degree Kelvin (1º K)
+	    //
+	    // One gram of pure liquid water has a volume of one centimeter cubed (1 cm3) or 
+	    // one milliliter (1 ml).
+	    
+	    // definitions
+	    final Unit<Energy> UNIT_KILO_WATT_HOUR = KILO(Units.WATT.multiply(Units.HOUR)).asType(Energy.class);
+	    final Unit<Energy> UNIT_CAL = Units.JOULE.multiply(RationalNumber.of(4184, 1000)); // as per definition
+	    final Unit<Volume> UNIT_MILLILITRE = MILLI(Units.LITRE);
+	    final Quantity<?> ENERGY_NEEDED_PER_MILLILITRE_AND_KELVIN =
+	            Quantities.getQuantity(1, UNIT_CAL.divide(Units.KELVIN).divide(UNIT_MILLILITRE));
 
-		final Quantity<Volume> volume = Quantities.getQuantity(1000, Units.LITRE);
-		System.out.println(String.format(resourceBundle.getString("what_work_is_required_to_heat_water"),
-				volume));
-		Quantity<Temperature> deltaT = Quantities.getQuantity(20, Units.KELVIN, RELATIVE);
-		Quantity<Energy> energy = (Quantity<Energy>) volume.multiply(deltaT).multiply(1000);
-		// TODO check if this could be avoided, see https://github.com/unitsofmeasurement/uom-demos/issues/91
-		Quantity<Energy> W = (energy.multiply(4200)).divide(3600);
-		final Unit<Energy> wattHour = W.getUnit();
-		SimpleUnitFormat.getInstance().label(wattHour, "Wh");
-		Quantity<Energy> kWh = W.to(KILO(wattHour));  
-		System.out.println(kWh);
-		Quantity<Power> kiloWatt = Quantities.getQuantity(9.5, KILO(Units.WATT));
-		Quantity<Time> time = (Quantity<Time>) kWh.divide(kiloWatt);
-		SimpleUnitFormat.getInstance().label(time.getUnit(), "h");
-		System.out.println(String.format(resourceBundle.getString("If_only_s_are_available_it_will_take_longer"),
-				kiloWatt));
-		System.out.println(String.format(resourceBundle.getString("Namely"), time));
+	    // customize formatting
+	    SimpleUnitFormat.getInstance().label(UNIT_KILO_WATT_HOUR, "kWh");
+
+	    // given
+		final Quantity<Volume> givenVolume = Quantities.getQuantity(1000, Units.LITRE);
+		final Quantity<Temperature> deltaT = Quantities.getQuantity(20, Units.CELSIUS, Scale.RELATIVE);
+
+        // question (1)
+        print("what_work_is_required_to_heat_water", givenVolume);
+		
+		// whats the energy requirement we need to put into given volume to heat it up a temperature amount of deltaT?
+		final Quantity<Energy> energyRequirementToHeatUpGivenVolume = 
+		        ENERGY_NEEDED_PER_MILLILITRE_AND_KELVIN
+		        .multiply(givenVolume) // 1000 Litre
+		        .multiply(deltaT) // 20ºC
+		        .asType(Energy.class)
+		        ; 
+		        
+		// convert to kWh
+		Quantity<Energy> kWh = energyRequirementToHeatUpGivenVolume
+		        .to(UNIT_KILO_WATT_HOUR);
+		
+		print("%s", kWh);
+		
+		// question (2)
+		// how log does it take to heat up given volume of water (delta 20ºC),
+		// if we put given  power (9.5kW) into our heater?
+		
+	    final Quantity<Power> givenPower = Quantities.getQuantity(9.5, KILO(Units.WATT));
+		
+	    final Quantity<Time> timeRequired = kWh.divide(givenPower).asType(Time.class)
+	            .to(Units.HOUR);
+		
+		print("If_only_s_are_available_it_will_take_longer", givenPower);
+		print("Namely", timeRequired);
 	}
+	
+	// -- LOCALIZED PRINTING
+	
+	static ResourceBundle resourceBundle;
+	
+	private static void print(final String format, final Object ...args) {
+	    if(resourceBundle==null) {
+	        // using German locale 
+	        resourceBundle = ResourceBundle.getBundle("Labels", Locale.GERMAN);
+	    }
+	    try {
+	        System.out.println(String.format(resourceBundle.getString(format), args));    
+	    } catch (Exception e) {
+	        // fallback to simple printing
+            System.out.println(String.format(format, args));
+        }
+	    
+	}
+	
 }
