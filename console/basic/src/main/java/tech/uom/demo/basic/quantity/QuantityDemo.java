a85743620c1fb288b839addb06b5a0a996caeecf
Werner Keil <werner.keil@gmx.net>
2023-03-15 11:44:42 +0100

    Merge branch 'master' of https://github.com/unitsofmeasurement/uom-demos.git

--- console/basic/src/main/java/tech/uom/demo/basic/quantity/QuantityDemo.java
+++ console/basic/src/main/java/tech/uom/demo/basic/quantity/QuantityDemo.java
@@ -4,12 +4,12 @@ import static tech.units.indriya.unit.Units.*;
 
 import java.math.BigDecimal;
 
-import javax.measure.MetricPrefix;
 import javax.measure.Quantity;
 import javax.measure.Unit;
 import javax.measure.quantity.Mass;
 import javax.measure.quantity.Energy;
 
+import tech.units.indriya.function.RationalNumber;
 import tech.units.indriya.quantity.Quantities;
 import tech.units.indriya.unit.Units;
 
@@ -32,18 +32,19 @@ public class QuantityDemo {
 	    Quantity<Mass> w1 = Quantities.getQuantity(12.5, KILOGRAM);
 
 	    // uses BasicUnit
-	    Quantity<Mass> w2 = (Quantity<Mass>) Quantities.getQuantity("12,5 kg");
+	    @SuppressWarnings("unchecked")
+		Quantity<Mass> w2 = (Quantity<Mass>) Quantities.getQuantity("12,5 kg");
 
 	    System.out.println(w1.equals(w2));
 	    //System.out.println(w2.equals(w1));
-	    
-		Quantity q1 = Quantities.getQuantity(0.0, Units.MOLE).add(Quantities.getQuantity(1.1, Units.MOLE));
-		Quantity q2 = Quantities.getQuantity(1.1, Units.MOLE);
-		System.out.println(q1.equals(q2));
-	    
+	    	    
 	    final Unit<Energy> UNIT_WATT_HOUR = Units.WATT.multiply(Units.HOUR).asType(Energy.class);
-		Quantity q3 = Quantities.getQuantity(0.0, UNIT_WATT_HOUR).add(Quantities.getQuantity(1.1, UNIT_WATT_HOUR));
-		Quantity q4 = Quantities.getQuantity(1.1, UNIT_WATT_HOUR);
+	    Quantity<?> q1 = Quantities.getQuantity(0.0, UNIT_WATT_HOUR).add(Quantities.getQuantity(1.1, UNIT_WATT_HOUR));
+	    Quantity<?> q2 = Quantities.getQuantity(1.1, UNIT_WATT_HOUR);
+		System.out.println(q1.equals(q2));
+		
+	    Quantity<?> q3 = Quantities.getQuantity(BigDecimal.ZERO, UNIT_WATT_HOUR).add(Quantities.getQuantity(BigDecimal.valueOf(1.1), UNIT_WATT_HOUR));
+	    Quantity<?> q4 = Quantities.getQuantity(RationalNumber.of(1.1), UNIT_WATT_HOUR);
 		System.out.println(q3.equals(q4));
 	}	
 }
--- console/java16/src/main/java/tech/uom/demo/java16/quantity/QuantityDemo.java
+++ console/java16/src/main/java/tech/uom/demo/java16/quantity/QuantityDemo.java
@@ -11,6 +11,7 @@ import javax.measure.Unit;
 import javax.measure.quantity.Energy;
 import javax.measure.quantity.Mass;
 
+import tech.units.indriya.function.RationalNumber;
 import tech.units.indriya.quantity.Quantities;
 import tech.units.indriya.unit.Units;
 
@@ -30,17 +31,21 @@ public class QuantityDemo {
 		//System.out.println("BD=D? " + BigDecimal.ONE.equals(new Double(1)));
 		
 	    // uses TransformedUnit
-	    Quantity<Mass> w1 = Quantities.getQuantity(12.5, KILOGRAM);
+	    var w1 = Quantities.getQuantity(12.5, KILOGRAM);
 
 	    // uses BasicUnit
-	    Quantity<Mass> w2 = (Quantity<Mass>) Quantities.getQuantity("12,5 kg");
+	    var w2 = (Quantity<Mass>) Quantities.getQuantity("12,5 kg");
 
 	    System.out.println(w1.equals(w2));
 	    //System.out.println(w2.equals(w1));
 	    
 	    final Unit<Energy> UNIT_WATT_HOUR = Units.WATT.multiply(Units.HOUR).asType(Energy.class);
-		Quantity q1 = Quantities.getQuantity(0.0, UNIT_WATT_HOUR).add(Quantities.getQuantity(1.1, UNIT_WATT_HOUR));
-		Quantity q2 = Quantities.getQuantity(1.1, UNIT_WATT_HOUR);
+		var q1 = Quantities.getQuantity(0.0, UNIT_WATT_HOUR).add(Quantities.getQuantity(1.1, UNIT_WATT_HOUR));
+		var q2 = Quantities.getQuantity(1.1, UNIT_WATT_HOUR);
 		System.out.println(q1.equals(q2));
+		
+	    var q3 = Quantities.getQuantity(BigDecimal.ZERO, UNIT_WATT_HOUR).add(Quantities.getQuantity(BigDecimal.valueOf(1.1), UNIT_WATT_HOUR));
+	    var q4 = Quantities.getQuantity(RationalNumber.of(1.1), UNIT_WATT_HOUR);
+		System.out.println(q3.equals(q4));
 	}	
 }
