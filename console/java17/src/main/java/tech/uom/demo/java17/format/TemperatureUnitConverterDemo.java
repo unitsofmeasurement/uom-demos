88bdffab7dc5ffca39e1dad0cc2b3bb51804920b
Werner Keil <werner.keil@gmx.net>
2024-10-04 20:43:05 +0200

    425: Add new Metric Prefixes to EBNFUnitFormat
    Task-Url: https://github.com/unitsofmeasurement/indriya/issues/425

--- console/java17/src/main/java/tech/uom/demo/java17/format/TemperatureUnitConverterDemo.java
+++ console/java17/src/main/java/tech/uom/demo/java17/format/TemperatureUnitConverterDemo.java
@@ -1,13 +1,15 @@
 package tech.uom.demo.java17.format;
 
+import static javax.measure.Quantity.Scale.*;
+
 import java.text.NumberFormat;
 
 import javax.measure.IncommensurableException;
-import javax.measure.Quantity.Scale;
 import javax.measure.UnconvertibleException;
 import javax.measure.UnitConverter;
 import javax.measure.spi.ServiceProvider;
 
+import tech.units.indriya.AbstractUnit;
 import tech.units.indriya.format.EBNFUnitFormat;
 import tech.units.indriya.format.NumberDelimiterQuantityFormat;
 import tech.units.indriya.quantity.Quantities;
@@ -24,9 +26,11 @@ public class TemperatureUnitConverterDemo {
         var dest = format.parse("°C");
         UnitConverter converter = null;
         UnitConverter converter2 = null;
+        UnitConverter converter3 = null;
         try {
 			converter = source.getConverterToAny(intermediary);
 			converter2 = dest.getConverterToAny(intermediary);
+			converter3 = ((AbstractUnit<?>)source2).getConverterToAny(dest, RELATIVE);
 		} catch (UnconvertibleException | IncommensurableException e) {
 			e.printStackTrace();
 		}
@@ -35,12 +39,16 @@ public class TemperatureUnitConverterDemo {
 			var converted = converter.convert(toK);
 			System.out.println(converted);			
 		}
+		if (converter3 != null) {
+			var converted = converter3.convert(10);
+			System.out.println(converted);
+		}
 		
-		var qs = Quantities.getQuantity(10, source, Scale.ABSOLUTE);
+		var qs = Quantities.getQuantity(10, source, ABSOLUTE);
 		System.out.println(format2.format(qs));
 		//var qs2 = Quantities.getQuantity(10, source2, Scale.ABSOLUTE);
 		//System.out.println(format2.format(qs2));
-		var qs3 = Quantities.getQuantity(1, intermediary,  Scale.ABSOLUTE);
+		var qs3 = Quantities.getQuantity(1, intermediary,  ABSOLUTE);
 		var qs4 = qs.multiply(qs3);
 		System.out.println(format2.format(qs4));
 		//var qs5 = qs.to(dest);
